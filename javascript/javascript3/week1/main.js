b14a18671f73fe768ef13b55fd322f9b6cc681e0
Omid-incarnation <mohammad.omidi1988@gmail.com>
2018-11-22 19:14:27 +0100

    cleanUp function

--- javascript/javascript3/week1/main.js
+++ javascript/javascript3/week1/main.js
@@ -13,14 +13,14 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
         console.log(movies);
 
         //Give each movie a tag: Good, Average, Bad. Based on the ratings
-        const over7 = movie => movie.rating >= 7; 
-        const between4To7 = movie => movie.rating >= 4 && movie.rating < 7;
-        const less4 = movie => movie.rating < 4;
+    
+        const getMoviesRatedOver7 = movie => movie.rating >= 7; 
+        const getMoviesRatedBetween4to7 = movie => movie.rating >= 4 && movie.rating < 7;
+        const getMoviesRatedLess4 = movie => movie.rating < 4;
 
-
-        const goodMovies = movies.filter(over7);
-        const averageMovies = movies.filter(between4To7);
-        const badMovies = movies.filter(less4);
+        const goodMovies = movies.filter(getMoviesRatedOver7);
+        const averageMovies = movies.filter(getMoviesRatedBetween4to7);
+        const badMovies = movies.filter(getMoviesRatedLess4);
 
         console.log(goodMovies);
         console.log(averageMovies);
@@ -31,7 +31,15 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
         document.getElementById('averageButton').addEventListener('click', showaverageMovies);
         document.getElementById('badButton').addEventListener('click', showBadMovies);
         
+        function cleanUpTheList(){
+            document.getElementById('goodMoviesUl').innerHTML = '';
+            document.getElementById('averageMoviesUl').innerHTML = '';
+            document.getElementById('badMoviesUl').innerHTML = '';
+            document.getElementById('80sMoviesUl').innerHTML = '';
+        }
+    
         function showGoodMovies(){
+            cleanUpTheList()
             let goodMoviesList = '';
             goodMovies.forEach((goodTitles) => {
                 goodMoviesList += `<li>${goodTitles.title}</li>`
@@ -40,6 +48,7 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
         };
     
         function showaverageMovies(){
+            cleanUpTheList()
             let averageMoviesList = '';
             averageMovies.forEach((averageTitles) => {
                 averageMoviesList += `<li>${averageTitles.title}</li>`
@@ -48,6 +57,7 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
         }
     
         function showBadMovies(){
+            cleanUpTheList()
             let badMoviesList = '';
             badMovies.forEach((badTitles) => {
                 badMoviesList += `<li>${badTitles.title}</li>`
@@ -55,25 +65,21 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
             document.getElementById('badMoviesUl').innerHTML = badMoviesList;
         }
     
-    
-    
         //Calculate the average rating of all the movies
+    
         let allRating = movies.map(getRating => getRating.rating);
         
         let averageOfRating = allRating.reduce((a, b) => a + b) / movies.length;
     
         console.log('Average Of Rating: ' + averageOfRating);
     
-    
         //Count the total number of Good, Average and Bad movies.
-        let totalMovies = goodMovies.length + averageMovies.length + badMovies.length;
-    
+            
+        let totalMovies = movies.length
         console.log('Total Number Of Movies: ' + totalMovies);
-    
     
         //following keywords: ["The", "dog", "who", "is", "not", "a", "man"]
 
-    
         const includesWords = function(title) {
             if(title.includes("The" || "dog" || "who" || "is" || "not" || "a" || "man") ){
                 return true;
@@ -86,7 +92,6 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
         })  
         console.log(filterMovies);
         
-    
         //movies made between 1980-1989
     
         const get80sMovies = function(movie){
@@ -97,7 +102,9 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
         console.log(_80sMovie);
     
         document.getElementById('80sButton').addEventListener('click', show80sMovies);
+        
         function show80sMovies(){
+            cleanUpTheList()
             let _80sMoviesList = '';
             _80sMovie.forEach((_80sTitles) => {
                 _80sMoviesList += `<li>${_80sTitles.title}</li>`
@@ -105,5 +112,3 @@ fetch('https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0a
             document.getElementById('80sMoviesUl').innerHTML = _80sMoviesList;
         }
 });
-
-
